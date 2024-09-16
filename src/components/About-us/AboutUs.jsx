a26b748cfb7a1870f4a3531f5a85804511ea3983
Aaditya Buchale <aadityabuchale15@gmail.com>
2024-09-16 22:07:46 +0530

    changed texts for home , about us and header

--- src/components/About-us/AboutUs.jsx
+++ src/components/About-us/AboutUs.jsx
@@ -6,68 +6,103 @@ import CountUp from "react-countup";
 import "./about.css";
 
 const AboutUs = () => {
-  return (
-    <section>
-      <Container>
-        <Row>
-          <Col lg="6" md="6">
-            <div className="about__img">
-              <img src={aboutImg} alt="" className="w-100" />
-            </div>
-          </Col>
+	return (
+		<section className='about-us'>
+			<Container>
+				<Row>
+					<Col lg='6' md='6'>
+						<div className='about__img'>
+							<img src={aboutImg} alt='' className='w-100' />
+						</div>
+					</Col>
 
-          <Col lg="6" md="6">
-            <div className="about__content">
-              <h2>About Us</h2>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Excepturi cupiditate animi deserunt libero nesciunt corporis
-                explicabo nobis ex quo molestiae!
-              </p>
+					<Col lg='6' md='6'>
+						<div className='about__content'>
+							<h2>About Us</h2>
+							<p>
+								We are an offline education platform based in
+								Maharashtra, offering free, high-quality courses
+								to students. Our mission is to empower learners
+								with government-certified education that
+								enhances their skills and provides official
+								recognition. Through our partnership with the
+								Indian Government, we ensure every student can
+								access valuable knowledge and certifications at
+								no cost, helping them unlock future
+								opportunities.
+							</p>
 
-              <div className="about__counter">
-                <div className=" d-flex gap-5 align-items-center">
-                  <div className="single__counter">
-                    <span className="counter">
-                      <CountUp start={0} end={25} duration={2} suffix="K" />
-                    </span>
+							<div className='about__counter'>
+								<div className=' d-flex gap-5 align-items-center'>
+									<div className='single__counter'>
+										<span className='counter'>
+											<CountUp
+												start={0}
+												end={5}
+												duration={2}
+												suffix='+'
+											/>
+										</span>
 
-                    <p className="counter__title">Completed Projects</p>
-                  </div>
+										<p className='counter__title'>
+											Total Centers
+										</p>
+									</div>
 
-                  <div className="single__counter">
-                    <span className="counter">
-                      <CountUp start={0} end={12} duration={2} suffix="M" />
-                    </span>
+									<div className='single__counter'>
+										<span className='counter'>
+											<CountUp
+												start={0}
+												end={1000}
+												duration={2}
+												suffix='+'
+											/>
+										</span>
 
-                    <p className="counter__title">Patient Around World</p>
-                  </div>
-                </div>
+										<p className='counter__title'>
+											Overall Students
+										</p>
+									</div>
+								</div>
 
-                <div className=" d-flex gap-5 align-items-center">
-                  <div className="single__counter">
-                    <span className="counter">
-                      <CountUp start={0} end={95} duration={2} suffix="M" />
-                    </span>
+								<div className=' d-flex gap-5 align-items-center'>
+									{/* <div className='single__counter'>
+										<span className='counter'>
+											<CountUp
+												start={0}
+												end={95}
+												duration={2}
+												suffix='M'
+											/>
+										</span>
 
-                    <p className="counter__title">Ideas Raised Funds</p>
-                  </div>
+										<p className='counter__title'>
+											Ideas Raised Funds
+										</p>
+									</div> */}
 
-                  <div className="single__counter">
-                    <span className="counter">
-                      <CountUp start={0} end={5} duration={2} suffix="K" />
-                    </span>
+									<div className='single__counter'>
+										<span className='counter'>
+											<CountUp
+												start={0}
+												end={10}
+												duration={2}
+												prefix='More than '
+											/>
+										</span>
 
-                    <p className="counter__title">Categories Served</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  );
+										<p className='counter__title'>
+											Subjects Served
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
 };
 
 export default AboutUs;
